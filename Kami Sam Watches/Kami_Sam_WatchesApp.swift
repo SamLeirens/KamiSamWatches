e894949382a63e899043541ab72221c6de3f6d43
Sam Leirens <sam.leirens@mediagenix.tv>
2026-07-06 22:24:12 +0200

    Initial Commit

---
+++ Kami Sam Watches/Kami_Sam_WatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kami_Sam_WatchesApp.swift
+//  Kami Sam Watches
+//
+//  Created by Sam Leirens on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Kami_Sam_WatchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
